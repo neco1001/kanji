cd68eb6ced4d574fbb7dbfa77c5be53f612e1a59
Kenichi Kaneko <kaneko.kenichi@bpsinc.jp>
2022-06-20 21:59:03 +0900

    読み仮名の位置を調整

--- app/pdf/test_pdf.rb
+++ app/pdf/test_pdf.rb
@@ -18,31 +18,50 @@ class TestPdf < Prawn::Document
       start_y_position = start_y_positions[i]
       case questions[i]
       when '1'
-        draw_text 'や', at: [start_x_position, start_y_position]
-        draw_text 'ま', at: [start_x_position, start_y_position - 40]
+        bounding_box([start_x_position, start_y_position + 20], width: 20, height: 40) do
+          text 'や', align: :center, valign: :center
+        end
+        bounding_box([start_x_position, start_y_position - 20], width: 20, height: 40) do
+          text 'ま', align: :center, valign: :center
+        end
       when '2'
-        draw_text 'か', at: [start_x_position, start_y_position]
-        draw_text 'わ', at: [start_x_position, start_y_position - 40]
+        bounding_box([start_x_position, start_y_position + 20], width: 20, height: 40) do
+          text 'か', align: :center, valign: :center
+        end
+        bounding_box([start_x_position, start_y_position - 20], width: 20, height: 40) do
+          text 'わ', align: :center, valign: :center
+        end
       when '3'
-        draw_text 'い', at: [start_x_position, start_y_position]
-        draw_text 'ち', at: [start_x_position, start_y_position - 40]
+        bounding_box([start_x_position, start_y_position + 20], width: 20, height: 40) do
+          text 'い', align: :center, valign: :center
+        end
+        bounding_box([start_x_position, start_y_position - 20], width: 20, height: 40) do
+          text 'ち', align: :center, valign: :center
+        end
       when '4'
-        draw_text 'に', at: [start_x_position, start_y_position]
+        bounding_box([start_x_position, start_y_position + 20], width: 20, height: 80) do
+          text 'に', align: :center, valign: :center
+        end
       when '5'
-        draw_text 'さ', at: [start_x_position, start_y_position]
-        draw_text 'ん', at: [start_x_position, start_y_position - 40]
+        bounding_box([start_x_position, start_y_position + 20], width: 20, height: 40) do
+          text 'さ', align: :center, valign: :center
+        end
+        bounding_box([start_x_position, start_y_position - 20], width: 20, height: 40) do
+          text 'ん', align: :center, valign: :center
+        end
       when '6'
-        draw_text 'あ', at: [start_x_position, start_y_position]
-        draw_text 'か', at: [start_x_position, start_y_position - 40]
-      end
-      bounding_box([start_x_position - 90, start_y_position + 20], width: 80, height: 80) do
-        stroke_bounds
-      end
-
-      if questions[i] == '6'
+        bounding_box([start_x_position, start_y_position + 20], width: 20, height: 40) do
+          text 'あ', align: :center, valign: :center
+        end
+        bounding_box([start_x_position, start_y_position - 20], width: 20, height: 40) do
+          text 'か', align: :center, valign: :center
+        end
         bounding_box([start_x_position - 90, start_y_position - 60], width: 80, height: 80) do
           text 'い', size: 50, align: :center, valign: :center
         end
+      end
+      bounding_box([start_x_position - 90, start_y_position + 20], width: 80, height: 80) do
+        stroke_bounds
       end
     end
   end
