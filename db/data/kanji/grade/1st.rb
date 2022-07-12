1522636b0b366c96c8e96e1a66b365381c5414b2
Kenichi Kaneko <kaneko.kenichi@bpsinc.jp>
2022-07-12 16:01:11 +0900

    1年生の漢字データを登録

--- db/data/kanji/grade/1st.rb
+++ db/data/kanji/grade/1st.rb
@@ -1,9 +1,550 @@
 FIRST_GRADE_KANJIS = [
-  { character: '山', school_year: 1, onyomi: ['サン'], kunyomi: [{ yomigana: 'やま', okurigana: nil }] },
-  { character: '川', school_year: 1, onyomi: [], kunyomi: [{ yomigana: 'かわ', okurigana: nil }] },
-  { character: '一', school_year: 1, onyomi: ['イチ'], kunyomi: [] },
-  { character: '二', school_year: 1, onyomi: ['ニ'], kunyomi: [] },
-  { character: '三', school_year: 1, onyomi: ['サン'], kunyomi: [] },
-  { character: '赤', school_year: 1, onyomi: [], kunyomi: [{ yomigana: 'あか', okurigana: 'い' }] },
-  { character: '見', school_year: 1, onyomi: [], kunyomi: [{ yomigana: 'み', okurigana: 'る' }, { yomigana: 'み', okurigana: 'える' }] }
+  {
+    character: '木', school_year: 1,
+    onyomi: ['ボク', 'モク'],
+    kunyomi: [
+      { yomigana: 'き', okurigana: nil }, { yomigana: 'こ', okurigana: nil }
+    ]
+  },
+  {
+    character: '大', school_year: 1,
+    onyomi: ['ダイ', 'タイ'],
+    kunyomi: [
+      { yomigana: 'おお', okurigana: 'きい' }
+    ]
+  },
+  {
+    character: '小', school_year: 1,
+    onyomi: ['ショウ'],
+    kunyomi: [
+      { yomigana: 'ちい', okurigana: 'さい' }, { yomigana: 'こ', okurigana: nil }, { yomigana: 'お', okurigana: nil }
+    ]
+  },
+  {
+    character: '一', school_year: 1,
+    onyomi: ['イチ', 'イツ'],
+    kunyomi: [
+      { yomigana: 'ひと', okurigana: 'つ' }
+    ]
+  },
+  {
+    character: '二', school_year: 1,
+    onyomi: ['ニ'],
+    kunyomi: [
+      { yomigana: 'ふた', okurigana: 'つ' }
+    ]
+  },
+  {
+    character: '三', school_year: 1,
+    onyomi: ['サン'],
+    kunyomi: [
+      { yomigana: 'みっ', okurigana: 'つ' }, { yomigana: 'み', okurigana: 'つ' }
+    ]
+  },
+  {
+    character: '四', school_year: 1,
+    onyomi: ['シ'],
+    kunyomi: [
+      { yomigana: 'よん', okurigana: nil }, { yomigana: 'よっ', okurigana: 'つ' }, { yomigana: 'よ', okurigana: 'つ' }, { yomigana: 'よ', okurigana: nil }
+    ]
+  },
+  {
+    character: '五', school_year: 1,
+    onyomi: ['ゴ'],
+    kunyomi: [
+      { yomigana: 'いつ', okurigana: 'つ' }, { yomigana: 'いつ', okurigana: nil }
+    ]
+  },
+  {
+    character: '六', school_year: 1,
+    onyomi: ['ロク'],
+    kunyomi: [
+      { yomigana: 'むっ', okurigana: 'つ' }, { yomigana: 'むい', okurigana: nil }
+    ]
+  },
+  {
+    character: '七', school_year: 1,
+    onyomi: ['シチ'],
+    kunyomi: [
+      { yomigana: 'なな', okurigana: nil }, { yomigana: 'なな', okurigana: 'つ' }, { yomigana: 'なの', okurigana: nil }
+    ]
+  },
+  {
+    character: '八', school_year: 1,
+    onyomi: ['シチ'],
+    kunyomi: [
+      { yomigana: 'なな', okurigana: nil }, { yomigana: 'なな', okurigana: 'つ' }, { yomigana: 'なの', okurigana: nil }
+    ]
+  },
+  {
+    character: '九', school_year: 1,
+    onyomi: ['キュウ', 'ク'],
+    kunyomi: [
+      { yomigana: 'ここの', okurigana: 'つ' }, { yomigana: 'ここの', okurigana: nil }
+    ]
+  },
+  {
+    character: '十', school_year: 1,
+    onyomi: ['ジュウ', 'ジ'],
+    kunyomi: [
+      { yomigana: 'とお', okurigana: nil }, { yomigana: 'と', okurigana: nil }
+    ]
+  },
+  {
+    character: '子', school_year: 1,
+    onyomi: ['シ', 'ス'],
+    kunyomi: [
+      { yomigana: 'こ', okurigana: nil }
+    ]
+  },
+  {
+    character: '空', school_year: 1,
+    onyomi: ['クウ'],
+    kunyomi: [
+      { yomigana: 'そら', okurigana: nil }, { yomigana: 'あ', okurigana: 'く' }, { yomigana: 'あ', okurigana: 'ける' }, { yomigana: 'から', okurigana: nil }
+    ]
+  },
+  {
+    character: '男', school_year: 1,
+    onyomi: ['ダン', 'ナン'],
+    kunyomi: [
+      { yomigana: 'おとこ', okurigana: nil }
+    ]
+  },
+  {
+    character: '女', school_year: 1,
+    onyomi: ['ジョ'],
+    kunyomi: [
+      { yomigana: 'おんな', okurigana: nil }
+    ]
+  },
+  {
+    character: '手', school_year: 1,
+    onyomi: ['シュ'],
+    kunyomi: [
+      { yomigana: 'て', okurigana: nil }
+    ]
+  },
+  {
+    character: '天', school_year: 1,
+    onyomi: ['テン'],
+    kunyomi: [
+      { yomigana: 'あま', okurigana: nil }
+    ]
+  },
+  {
+    character: '青', school_year: 1,
+    onyomi: ['セイ'],
+    kunyomi: [
+      { yomigana: 'あお', okurigana: nil }, { yomigana: 'あお', okurigana: 'い' }
+    ]
+  },
+  {
+    character: '見', school_year: 1,
+    onyomi: ['ケン'],
+    kunyomi: [
+      { yomigana: 'み', okurigana: 'る' }, { yomigana: 'み', okurigana: 'える' }, { yomigana: 'み', okurigana: 'せる' }
+    ]
+  },
+  {
+    character: '学', school_year: 1,
+    onyomi: ['ガク'],
+    kunyomi: [
+      { yomigana: 'まな', okurigana: 'ぶ' }
+    ]
+  },
+  {
+    character: '校', school_year: 1,
+    onyomi: ['コウ'],
+    kunyomi: []
+  },
+  {
+    character: '虫', school_year: 1,
+    onyomi: ['チュウ'],
+    kunyomi: [
+      { yomigana: 'むし', okurigana: nil }
+    ]
+  },
+  {
+    character: '文', school_year: 1,
+    onyomi: ['ブン', 'モン'],
+    kunyomi: []
+  },
+  {
+    character: '正', school_year: 1,
+    onyomi: ['セイ', 'ショウ'],
+    kunyomi: [
+      { yomigana: 'ただ', okurigana: 'しい' }, { yomigana: 'ただ', okurigana: 'す' }, { yomigana: 'まさ', okurigana: nil }
+    ]
+  },
+  {
+    character: '字', school_year: 1,
+    onyomi: ['ジ'],
+    kunyomi: []
+  },
+  {
+    character: '山', school_year: 1,
+    onyomi: ['サン'],
+    kunyomi: [
+      { yomigana: 'やま', okurigana: nil }
+    ]
+  },
+  {
+    character: '水', school_year: 1,
+    onyomi: ['スイ'],
+    kunyomi: [
+      { yomigana: 'みず', okurigana: nil }
+    ]
+  },
+  {
+    character: '雨', school_year: 1,
+    onyomi: ['ウ'],
+    kunyomi: [
+      { yomigana: 'あめ', okurigana: nil }, { yomigana: 'あま', okurigana: nil }
+    ]
+  },
+  {
+    character: '上', school_year: 1,
+    onyomi: ['ジョウ'],
+    kunyomi: [
+      { yomigana: 'うえ', okurigana: nil }, { yomigana: 'うわ', okurigana: nil }, { yomigana: 'かみ', okurigana: nil }, { yomigana: 'あ', okurigana: 'げる' }, { yomigana: 'あ', okurigana: 'がる' }, { yomigana: 'のぼ', okurigana: 'る' }
+    ]
+  },
+  {
+    character: '下', school_year: 1,
+    onyomi: ['カ', 'ゲ'],
+    kunyomi: [
+      { yomigana: 'した', okurigana: nil }, { yomigana: 'しも', okurigana: nil }, { yomigana: 'さ', okurigana: 'げる' }, { yomigana: 'さ', okurigana: 'がる' }, { yomigana: 'くだ', okurigana: 'る' }, { yomigana: 'お', okurigana: 'ろす' }, { yomigana: 'お', okurigana: 'りる' }
+    ]
+  },
+  {
+    character: '日', school_year: 1,
+    onyomi: ['ニチ', 'ジツ'],
+    kunyomi: [
+      { yomigana: 'ひ', okurigana: nil }, { yomigana: 'か', okurigana: nil }
+    ]
+  },
+  {
+    character: '火', school_year: 1,
+    onyomi: ['カ'],
+    kunyomi: [
+      { yomigana: 'ひ', okurigana: nil }
+    ]
+  },
+  {
+    character: '田', school_year: 1,
+    onyomi: ['デン'],
+    kunyomi: [
+      { yomigana: 'た', okurigana: nil }
+    ]
+  },
+  {
+    character: '川', school_year: 1,
+    onyomi: [],
+    kunyomi: [
+      { yomigana: 'かわ', okurigana: nil }
+    ]
+  },
+  {
+    character: '竹', school_year: 1,
+    onyomi: ['チク'],
+    kunyomi: [
+      { yomigana: 'たけ', okurigana: nil }
+    ]
+  },
+  {
+    character: '月', school_year: 1,
+    onyomi: ['ゲツ', 'ガツ'],
+    kunyomi: [
+      { yomigana: 'つき', okurigana: nil }
+    ]
+  },
+  {
+    character: '車', school_year: 1,
+    onyomi: ['シャ'],
+    kunyomi: [
+      { yomigana: 'くるま', okurigana: nil }
+    ]
+  },
+  {
+    character: '人', school_year: 1,
+    onyomi: ['ジン', 'ニン'],
+    kunyomi: [
+      { yomigana: 'ひと', okurigana: nil }
+    ]
+  },
+  {
+    character: '気', school_year: 1,
+    onyomi: ['キ', 'ケ'],
+    kunyomi: []
+  },
+  {
+    character: '本', school_year: 1,
+    onyomi: ['ホン'],
+    kunyomi: [
+      { yomigana: 'もと', okurigana: nil }
+    ]
+  },
+  {
+    character: '森', school_year: 1,
+    onyomi: ['シン'],
+    kunyomi: [
+      { yomigana: 'もり', okurigana: nil }
+    ]
+  },
+  {
+    character: '出', school_year: 1,
+    onyomi: ['シュツ'],
+    kunyomi: [
+      { yomigana: 'で', okurigana: 'る' }, { yomigana: 'だ', okurigana: 'す' }
+    ]
+  },
+  {
+    character: '中', school_year: 1,
+    onyomi: ['チュウ', 'ジュウ'],
+    kunyomi: [
+      { yomigana: 'なか', okurigana: nil }
+    ]
+  },
+  {
+    character: '町', school_year: 1,
+    onyomi: ['チョウ'],
+    kunyomi: [
+      { yomigana: 'まち', okurigana: nil }
+    ]
+  },
+  {
+    character: '入', school_year: 1,
+    onyomi: ['ニュウ'],
+    kunyomi: [
+      { yomigana: 'い', okurigana: 'る' }, { yomigana: 'い', okurigana: 'れる' }, { yomigana: 'はい', okurigana: 'る' }
+    ]
+  },
+  {
+    character: '名', school_year: 1,
+    onyomi: ['メイ', 'ミョウ'],
+    kunyomi: [
+      { yomigana: 'な', okurigana: nil }
+    ]
+  },
+  {
+    character: '夕', school_year: 1,
+    onyomi: [],
+    kunyomi: [
+      { yomigana: 'ゆう', okurigana: nil }
+    ]
+  },
+  {
+    character: '百', school_year: 1,
+    onyomi: ['ヒャク'],
+    kunyomi: []
+  },
+  {
+    character: '円', school_year: 1,
+    onyomi: ['エン'],
+    kunyomi: [
+      { yomigana: 'まる', okurigana: 'い' }
+    ]
+  },
+  {
+    character: '千', school_year: 1,
+    onyomi: ['セン'],
+    kunyomi: [
+      { yomigana: 'ち', okurigana: nil }
+    ]
+  },
+  {
+    character: '花', school_year: 1,
+    onyomi: ['カ'],
+    kunyomi: [
+      { yomigana: 'はな', okurigana: nil }
+    ]
+  },
+  {
+    character: '休', school_year: 1,
+    onyomi: ['キュウ'],
+    kunyomi: [
+      { yomigana: 'やす', okurigana: 'む' }, { yomigana: 'やす', okurigana: 'まる' }, { yomigana: 'やす', okurigana: 'める' }
+    ]
+  },
+  {
+    character: '金', school_year: 1,
+    onyomi: ['キン', 'コン'],
+    kunyomi: [
+      { yomigana: 'かね', okurigana: nil }, { yomigana: 'かな', okurigana: nil }
+    ]
+  },
+  {
+    character: '土', school_year: 1,
+    onyomi: ['ド', 'ト'],
+    kunyomi: [
+      { yomigana: 'つち', okurigana: nil }
+    ]
+  },
+  {
+    character: '糸', school_year: 1,
+    onyomi: ['シ'],
+    kunyomi: [
+      { yomigana: 'いと', okurigana: nil }
+    ]
+  },
+  {
+    character: '目', school_year: 1,
+    onyomi: ['モク'],
+    kunyomi: [
+      { yomigana: 'め', okurigana: nil }
+    ]
+  },
+  {
+    character: '玉', school_year: 1,
+    onyomi: ['ギョク'],
+    kunyomi: [
+      { yomigana: 'たま', okurigana: nil }
+    ]
+  },
+  {
+    character: '村', school_year: 1,
+    onyomi: ['ソン'],
+    kunyomi: [
+      { yomigana: 'むら', okurigana: nil }
+    ]
+  },
+  {
+    character: '白', school_year: 1,
+    onyomi: ['ハク'],
+    kunyomi: [
+      { yomigana: 'しろ', okurigana: nil }, { yomigana: 'しら', okurigana: nil }, { yomigana: 'しろ', okurigana: 'い' }
+    ]
+  },
+  {
+    character: '音', school_year: 1,
+    onyomi: ['オン'],
+    kunyomi: [
+      { yomigana: 'おと', okurigana: nil }, { yomigana: 'ね', okurigana: nil }
+    ]
+  },
+  {
+    character: '赤', school_year: 1,
+    onyomi: ['セキ'],
+    kunyomi: [
+      { yomigana: 'あか', okurigana: nil }, { yomigana: 'あか', okurigana: 'い' }, { yomigana: 'あか', okurigana: 'らむ' }, { yomigana: 'あか', okurigana: 'らめる' }
+    ]
+  },
+  {
+    character: '生', school_year: 1,
+    onyomi: ['セイ', 'ショウ'],
+    kunyomi: [
+      { yomigana: 'い', okurigana: 'きる' }, { yomigana: 'い', okurigana: 'かす' }, { yomigana: 'い', okurigana: 'ける' }, { yomigana: 'う', okurigana: 'まれる' }, { yomigana: 'う', okurigana: 'む' }, { yomigana: 'は', okurigana: 'える' }, { yomigana: 'は', okurigana: 'やす' }, { yomigana: 'なま', okurigana: nil }
+    ]
+  },
+  {
+    character: '耳', school_year: 1,
+    onyomi: [],
+    kunyomi: [
+      { yomigana: 'みみ', okurigana: nil }
+    ]
+  },
+  {
+    character: '王', school_year: 1,
+    onyomi: ['オウ'],
+    kunyomi: []
+  },
+  {
+    character: '口', school_year: 1,
+    onyomi: ['コウ', 'ク'],
+    kunyomi: [
+      { yomigana: 'くち', okurigana: nil }
+    ]
+  },
+  {
+    character: '年', school_year: 1,
+    onyomi: ['ネン'],
+    kunyomi: [
+      { yomigana: 'とし', okurigana: nil }
+    ]
+  },
+  {
+    character: '立', school_year: 1,
+    onyomi: ['リツ'],
+    kunyomi: [
+      { yomigana: 'た', okurigana: 'つ' }, { yomigana: 'た', okurigana: 'てる' }
+    ]
+  },
+  {
+    character: '草', school_year: 1,
+    onyomi: ['ソウ'],
+    kunyomi: [
+      { yomigana: 'くさ', okurigana: nil }
+    ]
+  },
+  {
+    character: '先', school_year: 1,
+    onyomi: ['セン'],
+    kunyomi: [
+      { yomigana: 'さき', okurigana: nil }
+    ]
+  },
+  {
+    character: '犬', school_year: 1,
+    onyomi: ['ケン'],
+    kunyomi: [
+      { yomigana: 'いぬ', okurigana: nil }
+    ]
+  },
+  {
+    character: '早', school_year: 1,
+    onyomi: ['ソウ'],
+    kunyomi: [
+      { yomigana: 'はや', okurigana: 'い' }, { yomigana: 'はや', okurigana: 'まる' }, { yomigana: 'はや', okurigana: 'める' }
+    ]
+  },
+  {
+    character: '貝', school_year: 1,
+    onyomi: [],
+    kunyomi: [
+      { yomigana: 'かい', okurigana: nil }
+    ]
+  },
+  {
+    character: '林', school_year: 1,
+    onyomi: ['リン'],
+    kunyomi: [
+      { yomigana: 'はやし', okurigana: nil }
+    ]
+  },
+  {
+    character: '右', school_year: 1,
+    onyomi: ['ウ', 'ユウ'],
+    kunyomi: [
+      { yomigana: 'みぎ', okurigana: nil }
+    ]
+  },
+  {
+    character: '足', school_year: 1,
+    onyomi: ['ソク'],
+    kunyomi: [
+      { yomigana: 'あし', okurigana: nil }, { yomigana: 'た', okurigana: 'りる' }, { yomigana: 'た', okurigana: 'る' }, { yomigana: 'た', okurigana: 'す' }
+    ]
+  },
+  {
+    character: '石', school_year: 1,
+    onyomi: ['セキ', 'シャク'],
+    kunyomi: [
+      { yomigana: 'いし', okurigana: nil }
+    ]
+  },
+  {
+    character: '左', school_year: 1,
+    onyomi: ['サ'],
+    kunyomi: [
+      { yomigana: 'ひだり', okurigana: nil }
+    ]
+  },
+  {
+    character: '力', school_year: 1,
+    onyomi: ['リョク', 'リキ'],
+    kunyomi: [
+      { yomigana: 'ちから', okurigana: nil }
+    ]
+  }
 ]
